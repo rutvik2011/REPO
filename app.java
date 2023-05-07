e49ef289b188217e9f3cfc460acfffc359a343e6
rutvik2011 <santhoshgopals9@gmail.com>
2023-05-07 17:14:44 +0530

    changes made by nani

--- app.java
+++ app.java
@@ -6,3 +6,32 @@ public class Main {
     System.out.println(z);
   }
 }
+changes made by me
+plugins {
+    id 'application' 
+}
+
+repositories {
+    mavenCentral() 
+}
+
+dependencies {
+    testImplementation 'org.junit.jupiter:junit-jupiter:5.9.1' 
+
+    implementation 'com.google.guava:guava:31.1-jre' 
+}
+
+application {
+    mainClass = 'demo.App' 
+}
+
+tasks.named('test') {
+    useJUnitPlatform() 
+}public class Main {
+  public static void main(String[] args) {
+    String x = "10";
+    int y = 20;
+    String z = x + y;
+    System.out.println(z);
+  }
+}
