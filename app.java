4d68a1fec8e8293147d15dbcc6eb27ec8c0fa6b8
rutvik2011 <santhoshgopals9@gmail.com>
2023-05-07 17:24:31 +0530

    changes by nani-2

--- app.java
+++ app.java
@@ -6,3 +6,17 @@ public class Main {
     System.out.println(z);
   }
 }
+
+cahanges madeby nani-2
+
+package demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
